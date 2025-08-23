73246085ff81b19cbe7ef9f62e62378246e9a7c9
Felipe <felipeferreira.tech@outlook.com>
2025-08-23 18:13:03 -0300

    Adicoinando vídeos na página

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ document.querySelectorAll(".menu-mobile a").forEach(link => {
     menuBtn.innerHTML = '<i class="bi bi-list"></i>';
   });
 });
+
+
+const videoContainer = document.getElementById("video-container");
+const projetos = document.querySelectorAll(".projeto");
+
+function abrirVideo(url) {
+  // cria player
+  videoContainer.innerHTML = `
+    <button class="close-btn">Fechar</button>
+    <video controls autoplay>
+      <source src="${url}" type="video/mp4">
+      Seu navegador não suporta vídeos.
+    </video>
+  `;
+
+  // exibe e faz scroll
+  videoContainer.style.display = "block";
+  videoContainer.scrollIntoView({ behavior: "smooth" });
+
+  // botão fechar
+  videoContainer.querySelector(".close-btn").addEventListener("click", () => {
+    videoContainer.style.display = "none";
+    videoContainer.innerHTML = "";
+  });
+}
+
+// adiciona evento em todos os projetos
+projetos.forEach(projeto => {
+  projeto.addEventListener("click", (e) => {
+    e.preventDefault();
+    const url = projeto.getAttribute("data-video");
+    abrirVideo(url);
+  });
+});
